c4b74d350f8a5066e17314412c5350eea1ce3571
Tomate0613 <69756782+tomate0613@users.noreply.github.com>
2024-11-26 13:09:46 +0100

    Add keybind to toggle area rendering and disable rendering by default

--- src/main/java/dev/doublekekse/area_lib/client/AreaLibClient.java
+++ src/main/java/dev/doublekekse/area_lib/client/AreaLibClient.java
@@ -1,19 +1,29 @@
 package dev.doublekekse.area_lib.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import dev.doublekekse.area_lib.data.AreaClientData;
 import dev.doublekekse.area_lib.packet.ClientboundAreaSyncPacket;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
 
 public class AreaLibClient implements ClientModInitializer {
+    static boolean renderAreas = false;
 
     @Override
     public void onInitializeClient() {
         ClientPlayNetworking.registerGlobalReceiver(ClientboundAreaSyncPacket.TYPE, ClientboundAreaSyncPacket::handle);
 
         WorldRenderEvents.AFTER_ENTITIES.register((context) -> {
+            if (!renderAreas) {
+                return;
+            }
+
             var gameMode = Minecraft.getInstance().gameMode;
 
             if (gameMode == null) {
@@ -45,6 +55,19 @@ public class AreaLibClient implements ClientModInitializer {
             }
 
             poseStack.popPose();
+        });
+
+        var keyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "area_lib.key.toggle_areas",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN,
+                "area_lib.category.area_lib"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.consumeClick()) {
+                renderAreas = !renderAreas;
+            }
         });
     }
 }
