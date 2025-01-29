145f6de65e508b62b70f86287c32e23fb965d4af
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-01-29 20:57:43 +0100

    Add some javadoc

--- src/main/java/dev/doublekekse/area_lib/Area.java
+++ src/main/java/dev/doublekekse/area_lib/Area.java
@@ -8,21 +8,66 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
 public interface Area extends BVHItem {
+    /**
+     * Checks whether the given entity is within the area.
+     *
+     * @param entity the entity to check
+     * @return true if the entity is inside the area, false otherwise
+     */
     default boolean contains(Entity entity) {
         return contains(entity.level(), entity.position());
     }
 
+    /**
+     * Saves the area's data to a {@link CompoundTag}.
+     *
+     * @return a {@link CompoundTag} containing the saved state of the area
+     */
     CompoundTag save();
 
+    /**
+     * Loads the area's data from a {@link CompoundTag}.
+     *
+     * @param compoundTag the tag containing saved area data
+     */
     void load(CompoundTag compoundTag);
 
+    /**
+     * Sets the color used to render this area.
+     *
+     * @param r the red component (0.0 - 1.0)
+     * @param g the green component (0.0 - 1.0)
+     * @param b the blue component (0.0 - 1.0)
+     */
     void setColor(float r, float g, float b);
 
+    /**
+     * Renders this area in the world.
+     *
+     * @param context   the world render context
+     * @param poseStack the pose stack used for transformations
+     */
     void render(WorldRenderContext context, PoseStack poseStack);
 
+    /**
+     * Gets the unique type identifier of this type of area.
+     *
+     * @return the type as a {@link ResourceLocation}
+     */
     ResourceLocation getType();
 
+    /**
+     * Gets the priority of the area. Priority can be used to determine rendering order
+     * or processing importance.
+     *
+     * @return the priority value
+     */
     int getPriority();
 
+    /**
+     * Sets the priority of the area.
+     *
+     * @param priority the new priority value
+     */
     void setPriority(int priority);
 }
--- src/main/java/dev/doublekekse/area_lib/bvh/BVHItem.java
+++ src/main/java/dev/doublekekse/area_lib/bvh/BVHItem.java
@@ -7,7 +7,21 @@ import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 public interface BVHItem {
+    /**
+     * Checks whether the given position in the specified level is contained within the item.
+     *
+     * @param level    the level (world) to check in
+     * @param position the position to check
+     * @return true if the position is within the item, false otherwise
+     */
     boolean contains(Level level, Vec3 position);
 
+    /**
+     * Gets the bounding box of the item, if not empty. The bounding box is used for
+     * spatial partitioning and optimization in BVH structures.
+     *
+     * @param savedData the saved area data that may influence the bounding box
+     * @return the bounding box as an {@link AABB}, or null if empty
+     */
     @Nullable AABB getBoundingBox(AreaSavedData savedData);
 }
