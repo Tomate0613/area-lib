30cd3e4016e29450da94b0e3cfa879c2256457cd
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-02-25 22:33:45 +0100

    Remove unused import

--- src/main/java/dev/doublekekse/area_lib/bvh/BVHItem.java
+++ src/main/java/dev/doublekekse/area_lib/bvh/BVHItem.java
@@ -3,7 +3,6 @@ package dev.doublekekse.area_lib.bvh;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
 
 public interface BVHItem {
     /**
