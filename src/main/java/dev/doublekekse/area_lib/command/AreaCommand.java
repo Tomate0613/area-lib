06f17062c521676d7f079655db07554d1c3a3089
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-02-14 18:51:06 +0100

    fix replace_shape adding the area to every composite area

--- src/main/java/dev/doublekekse/area_lib/command/AreaCommand.java
+++ src/main/java/dev/doublekekse/area_lib/command/AreaCommand.java
@@ -236,12 +236,15 @@ public class AreaCommand {
             }
         }
 
-        savedData.remove(null, previousArea);
         savedData.put(null, area);
 
         for (var compositeArea : compositeAreas) {
-            compositeArea.addSubArea(null, area);
+            if(compositeArea.hasSubArea(previousArea)) {
+                compositeArea.addSubArea(null, area);
+            }
         }
+
+        savedData.remove(null, previousArea);
 
         area.copyComponentsFrom(server, previousArea);
 
