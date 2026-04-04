bfa110d0f040039366f8f6f0aadd8aff97d731df
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-04-04 17:47:54 +0200

    mark AreaDataComponentType constructor as internal

--- src/main/java/dev/doublekekse/area_lib/component/AreaDataComponentType.java
+++ src/main/java/dev/doublekekse/area_lib/component/AreaDataComponentType.java
@@ -2,6 +2,9 @@ package dev.doublekekse.area_lib.component;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.resources.Identifier;
+import org.jetbrains.annotations.ApiStatus;
 
 public record AreaDataComponentType<T>(Identifier id, Codec<T> codec, boolean tracking) {
+    @ApiStatus.Internal
+    public AreaDataComponentType {}
 }
