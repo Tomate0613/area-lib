4d28daace646275f62436b3f608aa6d47d0c308a
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-04-08 16:55:10 +0200

    make composite area an interface

--- src/main/java/dev/doublekekse/area_lib/areas/CompositeArea.java
+++ src/main/java/dev/doublekekse/area_lib/areas/CompositeArea.java
@@ -1,88 +1,13 @@
 package dev.doublekekse.area_lib.areas;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import dev.doublekekse.area_lib.Area;
-import dev.doublekekse.area_lib.bvh.LazyAreaBVHTree;
-import dev.doublekekse.area_lib.data.AreaSavedData;
-import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderContext;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.Identifier;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-
-public abstract class CompositeArea extends Area {
-    protected LazyAreaBVHTree areas;
-
-    int priority = 0;
-
-    public CompositeArea(AreaSavedData savedData, Identifier id, LazyAreaBVHTree areas) {
-        super(savedData, id);
-
-        this.areas = areas;
-    }
-
-    public CompositeArea(AreaSavedData savedData, Identifier id) {
-        super(savedData, id);
-    }
-
-    public void addSubArea(@Nullable MinecraftServer server, Area area) {
-        if (area instanceof CompositeArea) {
-            throw new IllegalArgumentException("Sub areas may not be composite areas");
-        }
-
-        areas.add(area.getId());
-        invalidate(server);
-    }
-
-    public void removeSubArea(@Nullable MinecraftServer server, Area area) {
-        areas.remove(area.getId());
-        invalidate(server);
-    }
-
-    public boolean hasSubArea(Area area) {
-        return areas.listAllAreas().contains(area);
-    }
-
-    public List<Area> getAreasContaining(Level level, Vec3 position) {
-        return areas.findAreasContaining(level, position);
-    }
-
-    public List<Area> getAreasContaining(Entity entity) {
-        return getAreasContaining(entity.level(), entity.position());
-    }
-
-    @Override
-    public void load(CompoundTag compoundTag) {
-        super.load(compoundTag);
-
-        areas = new LazyAreaBVHTree(savedData);
-        areas.load(compoundTag.getCompound("areas").get());
-
-        priority = compoundTag.getInt("priority").orElse(0);
-    }
-
-    @Override
-    public CompoundTag save() {
-        var compoundTag = super.save();
-
-        compoundTag.put("areas", areas.save());
-        compoundTag.putInt("priority", priority);
-
-        return compoundTag;
-    }
-
-    @Override
-    public @Nullable AABB getBoundingBox() {
-        return areas.getBoundingBox();
-    }
-
-    @Override
-    public void render(LevelRenderContext context, PoseStack poseStack, Identifier Identifier) {
-    }
+@ApiStatus.Experimental
+public interface CompositeArea {
+    void addSubArea(@Nullable MinecraftServer server, Area area);
+    void removeSubArea(@Nullable MinecraftServer server, Area area);
+    boolean hasSubArea(Area area);
 }
--- src/main/java/dev/doublekekse/area_lib/areas/UnionArea.java
+++ src/main/java/dev/doublekekse/area_lib/areas/UnionArea.java
@@ -1,15 +1,26 @@
 package dev.doublekekse.area_lib.areas;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.doublekekse.area_lib.Area;
 import dev.doublekekse.area_lib.AreaLib;
 import dev.doublekekse.area_lib.bvh.LazyAreaBVHTree;
 import dev.doublekekse.area_lib.data.AreaSavedData;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderContext;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
-public class UnionArea extends CompositeArea {
+public class UnionArea extends Area implements CompositeArea {
+    protected LazyAreaBVHTree areas;
+
     public UnionArea(AreaSavedData savedData, Identifier id, LazyAreaBVHTree areas) {
-        super(savedData, id, areas);
+        super(savedData, id);
+
+        this.areas = areas;
     }
 
     public UnionArea(AreaSavedData savedData, Identifier id) {
@@ -24,5 +35,53 @@ public class UnionArea extends CompositeArea {
     @Override
     public Identifier getType() {
         return AreaLib.id("union");
+    }
+
+    @Override
+    public void addSubArea(@Nullable MinecraftServer server, Area area) {
+        if (area instanceof CompositeArea) {
+            throw new IllegalArgumentException("Sub areas may not be composite areas");
+        }
+
+        areas.add(area.getId());
+        invalidate(server);
+    }
+
+    @Override
+    public void removeSubArea(@Nullable MinecraftServer server, Area area) {
+        areas.remove(area.getId());
+        invalidate(server);
+    }
+
+    @Override
+    public boolean hasSubArea(Area area) {
+        return areas.listAllAreas().contains(area);
+    }
+
+    @Override
+    public void render(LevelRenderContext context, PoseStack poseStack, Identifier dimension) {
+
+    }
+
+    @Override
+    public void load(CompoundTag compoundTag) {
+        super.load(compoundTag);
+
+        areas = new LazyAreaBVHTree(savedData);
+        areas.load(compoundTag.getCompound("areas").get());
+    }
+
+    @Override
+    public CompoundTag save() {
+        var compoundTag = super.save();
+
+        compoundTag.put("areas", areas.save());
+
+        return compoundTag;
+    }
+
+    @Override
+    public @Nullable AABB getBoundingBox() {
+        return areas.getBoundingBox();
     }
 }
